68ae1c93f34c081abf53b19182f7ecdfff8101ef
Ansgar Prause <ansgar.prause@convit.de>
2021-06-08 22:07:53 +0200

    Trigger Netlify build hook on post update

--- functions/discourse-webhook.ts
+++ functions/discourse-webhook.ts
@@ -1,21 +1,27 @@
 import { Handler } from "@netlify/functions";
 import { createHmac } from "crypto";
+import fetch from "node-fetch";
 
 interface Payload {
-  post: {
-    post_number: number;
+  post?: {
+    id?: number;
+    post_number?: number;
   };
 }
 
-const handler: Handler = async (event, context) => {
+const handler: Handler = async (event) => {
   const headerName = "X-Discourse-Event-Signature";
   const [, expected] =
     Object.entries(event.headers).find(
       ([key]) => key.toLowerCase() === headerName.toLowerCase()
     ) ?? [];
 
-  const hmac = createHmac("sha256", process.env.DISCOURSE_WEBHOOK_SECRET);
-  hmac.update(event.body);
+  const discourseWebhookSecret = process?.env?.DISCOURSE_WEBHOOK_SECRET;
+  if (!discourseWebhookSecret) {
+    throw new Error("Missing DISCOURSE_WEBHOOK_SECRET");
+  }
+  const hmac = createHmac("sha256", discourseWebhookSecret);
+  hmac.update(event.body ?? "");
   const hashedPayload = hmac.digest("hex");
   const actual = `sha256=${hashedPayload}`;
 
@@ -25,15 +31,40 @@ const handler: Handler = async (event, context) => {
     };
   }
 
-  const payload = JSON.parse(event.body) as Payload;
-
-  if (payload.post.post_number === 1) {
-    console.log("Erster Post", payload);
-  } else {
-    console.log("Anderer Post", payload);
+  let payload: Payload | undefined;
+  try {
+    payload = JSON.parse(event.body ?? "");
+  } catch (error) {
+    return {
+      statusCode: 400,
+    };
   }
 
-  console.log(hmac.digest("hex"));
+  const postId = payload?.post?.id;
+  console.info("Received webhook request", { postId });
+
+  if (payload?.post?.post_number === 1) {
+    // This is the initial post of the topic
+
+    const discourseBuildHook = process?.env?.DISCOURSE_WEBHOOK_SECRET;
+    if (!discourseBuildHook) {
+      throw new Error("Missing DISCOURSE_BUILD_HOOK");
+    }
+    const response = await fetch(discourseBuildHook);
+    if (!response.ok) {
+      console.error("Triggering build failed", {
+        status: response.status,
+        body: await response.text(),
+      });
+      return {
+        statusCode: 502,
+      };
+    }
+  } else {
+    console.info(
+      "This was not the initial post of the topic. It cannot contain an event, so we are ignoring it."
+    );
+  }
 
   return {
     statusCode: 200,
